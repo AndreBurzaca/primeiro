de7f4937015e0b616c393e832561ec481efb250e
AndreBurzaca <78853391+AndreBurzaca@users.noreply.github.com>
2021-02-10 09:10:39 -0300

    Update Principal.java
    Alteração na linha 28 comentario

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -1,31 +1,32 @@
 /**
  * Este programa mostra ao aluno:
- * 1) Que um programa Java é construido dendo de um projeto Java
- * 2) Todo programa Java deve ter uma classe com um método main
- * 3) Como exibir informações no terminal (console) do Java. 
+ * 1) Que um programa Java Ã© construido dendo de um projeto Java
+ * 2) Todo programa Java deve ter uma classe com um mÃ©todo main
+ * 3) Como exibir informaÃ§Ãµes no terminal (console) do Java. 
  * 4) Como ler uma String do terminal (console) do Java.
  */
 package meuPrimeiroProgramaJava;
 import java.util.Scanner;
 
 /**
- * @author André Burzaca Lopes
+ * @author AndrÃ© Burzaca Lopes
  * Data: 03/02/2021
  * 
  */
-public class Principal { //Classe que contém o método principal.
+public class Principal { //Classe que contÃ©m o mÃ©todo principal.
 	public static void main(String[] args) {
-		System.out.println("Você sabe quem é a melhor namorada do mundo?"); //Uso do println
+		System.out.println("VocÃª sabe quem Ã© a melhor namorada do mundo?"); //Uso do println
 	
 		// Ler uma string do console usando a biblioteca Scanner.
 		Scanner in = new Scanner (System.in);
 		String nome = in.nextLine();
 		
 		//Exibir uma string usando printf.
-		System.out.printf("A Milena a namorada do André ué");
+		System.out.printf("A Milena a namorada do AndrÃ© uÃ©");
 		in.close();
 		
-		System.out.printf("Pão");
+		// Ele gosta de pÃ£o
+		System.out.printf("PÃ£o");
 		
 	}
 
